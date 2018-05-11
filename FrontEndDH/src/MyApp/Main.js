50ef8881d84ada285616c4a1551f59fa89f5eea7
Dianne <diannehodnett@gmail.com>
2018-05-10 22:37:19 -0600

    working React App with Senator data

--- FrontEndDH/src/MyApp/Main.js
+++ FrontEndDH/src/MyApp/Main.js
@@ -6,10 +6,45 @@ import CreateFl from './4-CreateFloors'
 import UpdateFl from './5-UpdateFloors'
 import Style from './Style.css'
 import Template from './Template'
+import SenData from './senators'
 
-class Main extends Component {
-    render() {
-        return (
+export default class Main extends React.Component{
+    state = { showpol: false, senators: [] }
+
+    render(){
+
+        const FilterRepublicans = () => {
+            const RepSen = SenData.filter( (politician) => { return politician.party === "Republican"  })
+            this.setState({ senators: RepSen , showpol: true })
+        }
+
+
+        const FilterDemocrats = () => {
+            const DemSen = SenData.filter((politician) => { return politician.party === "Democrat" })
+            this.setState({ senators: DemSen , showpol: true })
+        }
+
+
+        const FilterUtahSenators = () => {
+            const UtahSen = SenData.filter( (politician) => { return politician.state === "UT"  } )
+            this.setState({ senators: UtahSen , showpol: true })
+        }
+
+
+        const FilterUtahSenior = () => {
+            const Senior = SenData.filter( (politician) => { return politician.description === "Senior Senator from Utah"  })
+            this.setState({ senators: Senior , showpol: true })
+        }
+
+
+        const RenderToScreen = () => {
+            console.clear()
+            console.log(this.state.senators)
+            return this.state.senators.map((politician) => { return <Template key={politician.phone} info={politician} />  })
+        }
+
+
+            return(
             <div className="Main">
             <header className="Main-header">
             <h1 className="Main-title">Apartment Unit Mix Analysis</h1>
@@ -23,9 +58,21 @@ class Main extends Component {
                 <li>Then graph the information in the cluster.</li>
             </ul>
         </p>
-        </div>
-        );
+
+
+            <div>
+                <button className="MyButton" onClick={ FilterRepublicans} >Republicans</button>
+                <button className="MyButton" onClick={ FilterDemocrats} >Democrats</button>
+                <button className="MyButton" onClick={ FilterUtahSenators } >Utah Senators</button>
+                <button className="MyButton" onClick={ FilterUtahSenior } >Utah Senior Senator</button>
+            </div>
+
+
+            <hr/>
+
+            { this.state.showpol ? React.createElement( RenderToScreen ) : <div>Click a Button</div> }
+
+            </div>
+        )
     }
 }
-
-export default Main;
